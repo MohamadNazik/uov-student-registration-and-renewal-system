26be745ffb71930b31324d5f0f0f4c5ce97a5b4a
Nazik <shmnazik79@gmail.com>
2025-01-06 11:53:08 +0530

    Missed files added from previous repo

--- student-frontend/src/pages/RegistrationProcess/A1Form_Part01.jsx
+++ student-frontend/src/pages/RegistrationProcess/A1Form_Part01.jsx
@@ -1,7 +1,237 @@
 import React from "react";
+import uov_logo from "../../assets/uov_logo.png";
+import Y_Image from "../../assets/DP.jpg";
 
 function A1Form_Part01() {
-  return <div>A1Form_Part01</div>;
+
+  return (
+    <>
+      <div className="bg-white md:block sm:flex m-2 sm:m-5 xl:ml-8 p-2 sm:p-7 xl:p-10 shadow-md rounded-lg  justify-center">
+        <div className=" flex text-left items-center  h-screenitems-center text-sm md:text-lg border border-black rounded-sm focus:outline-1 xl:ml-20 focus:outline-black  px-2 py-1 w-[200px] h-[25px] md:w-[300px] mb-4 sm:mb-6 justify-center">
+          A1 - Enrollment Form</div>
+        <header className="mb-4 sm:flex block justify-between sm:mb-5">
+          <img
+            src={uov_logo}
+            alt="University Logo"
+            className="w-[60px] sm:w-[130px] sm:h-[130px] mx-auto mb-2"
+          />
+          <div className="block justify-center text-center mt-0 md:mt-4">
+            <h1 className="text-xl sm:text-lg md:text-2xl xl:text-2xl font-semibold">UNIVERSITY OF VAVUNIYA</h1>
+            <p className="text-sm sm:text-lg xl:text-2xl text-gray-600">Personal Data of Students</p>
+            <p className="text-sm sm:text-lg xl:text-2xl text-gray-600">
+              Student Admission for the Academic Year 2022/23
+            </p></div>
+          <img
+            src={Y_Image}
+            alt="your Image"
+            className="w-[50px] sm:w-[120px] md:h-[130px] mx-auto mb-2 mt-2 md:mt-0 border border-black rounded-lg"
+          />
+        </header>
+
+
+        <form
+          className="flex flex-col items-center gap-5">
+          {/* Enrollment Number */}
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              1. Enrollment No :-
+            </label>
+            <input
+              type="text"
+              name="enrollment_no"
+              className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
+            />
+          </div>
+
+          {/* Name */}
+
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              2. Name :-
+            </label>
+          </div>
+
+          <div className="flex flex-col gap-2 sm:gap-4 ml-8">
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (i). Title :-
+              </label>
+              <div className="flex gap-2 sm:gap-7 ml-9">
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Rev"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Rev.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Mr"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Mr.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Mrs"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Mrs.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Miss"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Miss.
+                  </span>
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-sm xl:text-lg font-medium">
+                (other Please specify)
+              </label>
+              <input
+                type="text"
+                name="title"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (ii). Name with Initials :-
+              </label>
+              <input
+                type="text"
+                name="name_with_initials"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (iii). Name denoted by Initials :- 
+              </label>
+              <input
+                type="text"
+                name="name_denoted_by_initials"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+
+          </div>
+          {/* Address */}
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              2. Address :-
+            </label>
+          </div>
+
+          <div className="flex flex-col gap-2 sm:gap-4 ml-8">
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (i). Permanent Address :-
+              </label>
+              <textarea
+                type="text"
+                name="permanent_address"
+                rows={3}
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1 "
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (ii). province :-
+              </label>
+              <input
+                type="text"
+                name="province"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (ii). District :-
+              </label>
+              <input
+                type="text"
+                name="district"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (iii). Divisional Secretariat :-
+              </label>
+              <input
+                type="text"
+                name="divisional_secretariat"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (iv). National Identity Card No :-
+              </label>
+              <input
+                type="text"
+                name="divisional_secretariat"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (v). Mobile No :-
+              </label>
+              <input
+                type="text"
+                name="mobile_no"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (vi). Email Address :-
+              </label>
+              <input
+                type="text"
+                name="adddress"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+
+          </div>
+
+
+          {/* Next Button */}
+          <button
+            type="button"
+            className=" bg-green-600 text-white py-2 px-4 rounded-xl shadow hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+          >
+            NEXT
+          </button>
+        </form>
+      </div >
+
+
+
+    </>
+  );
 }
 
 export default A1Form_Part01;
--- student-frontend/src/pages/RegistrationProcess/A1Form_Part02.jsx
+++ student-frontend/src/pages/RegistrationProcess/A1Form_Part02.jsx
@@ -1,7 +1,293 @@
+{/* A1 form part b*/}
 import React from "react";
+import SecondaryButton from "../../components/SecondaryButton";
 
 function A1Form_Part02() {
-  return <div>A1Form_Part02</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white m-2 sm:m-5 xl:m-8 p-4 sm:p-7 xl:p-10 rounded-lg flex flex-col gap-5"
+    >
+      {/* Education Qualification */}
+      <div className="flex flex-col gap-4">
+        <h4 className="text-lg sm:text-xl xl:text-2xl font-bold">
+          4. EDUCATION QUALIFICATION
+        </h4>
+
+        {/* Year of GCE (A/L) Examination */}
+        <div className="flex gap-2 items-center">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-12">
+            i. Year of the GCE (A/L) Examination :-
+          </label>
+          <input
+            type="text"
+            name="yearOfALExamination"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+          />
+        </div>
+
+        {/* Index No of GCE (A/L) Examination */}
+        <div className="flex gap-2 items-center">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-12">
+            ii. Index No of the GCE (A/L) Examination :-
+          </label>
+          <input
+            type="text"
+            name="indexOfALExamination"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+          />
+        </div>
+
+        {/* Average Z Score */}
+        <div className="flex gap-2 items-center">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-12">
+            iii. Average Z Score :-
+          </label>
+          <input
+            type="text"
+            name="avgZscore"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+          />
+        </div>
+
+        {/* GCE (A/L) Examination */}
+        <div className="flex flex-col gap-4">
+          <h5 className="text-sm sm:text-lg xl:text-xl font-medium ml-12">
+            iv. GCE (A/L) Examination :-
+          </h5>
+
+          {/* Subject & Result Fields */}
+          <div className="flex gap-2 items-center ml-16">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-1">
+             Subject :-
+          </label>
+           <input
+            type="text"
+            name="subject1"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+           />
+         <label className="text-sm sm:text-lg xl:text-xl font-medium ml-5">
+            Result :-
+         </label>
+         <select
+           name="result1"
+          className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[100px] sm:w-[150px] xl:w-[200px]"
+         >
+          <option value=""></option>
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+          <option value="S">S</option>
+          <option value="F">F</option>
+        </select>
+      </div>
+            
+     <div className="flex gap-2 items-center ml-16">
+      <label className="text-sm sm:text-lg xl:text-xl font-medium ml-1">
+         Subject :-
+      </label>
+      <input
+        type="text"
+        name="subject2"
+        className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+      />
+      <label className="text-sm sm:text-lg xl:text-xl font-medium ml-5">
+         Result :-
+      </label>
+       <select
+        name="result2"
+        className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[100px] sm:w-[150px] xl:w-[200px]"
+       >
+       <option value=""></option>
+       <option value="A">A</option>
+       <option value="B">B</option>
+       <option value="C">C</option>
+       <option value="S">S</option>
+       <option value="F">F</option>
+      </select>
+    </div>
+     <div className="flex gap-2 items-center ml-16">
+     <label className="text-sm sm:text-lg xl:text-xl font-medium ml-1">
+        Subject :-
+     </label>
+       <input
+         type="text"
+         name="subject3"
+         className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+       />
+      <label className="text-sm sm:text-lg xl:text-xl font-medium ml-5">
+          Result :-
+      </label>
+       <select
+        name="result3"
+        className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[100px] sm:w-[150px] xl:w-[200px]"
+       >
+         <option value=""></option>
+         <option value="A">A</option>
+         <option value="B">B</option>
+         <option value="C">C</option>
+         <option value="S">S</option>
+         <option value="F">F</option>
+       </select>
+     </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col gap-5">
+  <h4 class="text-lg sm:text-xl xl:text-2xl font-bold">
+     5. DETAILS OF CITIZENSHIP
+   </h4>
+
+    {/* race */}
+  <div class="flex flex-col gap-2" >
+    <div class="flex items-center gap-4">
+      <label class="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+        i. Race :-
+      </label>
+      <label class="flex items-center gap-1">
+        <input type="radio" name="race" value="Sinhala" class="border-gray-400" />
+        <span class="text-sm sm:text-base xl:text-lg font-medium">Sinhala</span>
+      </label>
+      <label class="flex items-center gap-1">
+        <input type="radio" name="race" value="Tamil" class="border-gray-400" />
+        <span class="text-sm sm:text-base xl:text-lg font-medium">Tamil</span>
+      </label>
+      <label class="flex items-center gap-1">
+        <input type="radio" name="race" value="Muslim" class="border-gray-400" />
+        <span class="text-sm sm:text-base xl:text-lg font-medium">Muslim</span>
+      </label>
+      <label class="flex items-center gap-1">
+        <input type="radio" name="race" value="Others" class="border-gray-400" />
+        <span class="text-sm sm:text-base xl:text-lg font-medium">Others</span>
+      </label>
+    </div>
+      {/* if others */}
+      <div className="flex gap-2 items-center ml-10">
+      <label className="text-xs  ml-1">
+         (PI specify )
+      </label>
+      <input
+        type="text"
+        name="anyother"
+        className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+      />
+       </div>
+  </div>
+
+    {/* Gender */}
+    <div class="flex items-center gap-4">
+     <label class="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+       ii. Gender :-
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="gender" value="Male" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Male</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="gender" value="Female" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Female</span>
+    </label>
+  </div>
+
+    {/*civil status*/}
+    <div class="flex items-center gap-4">
+     <label class="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+      iii. Civil Status :-
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="civilStatus" value="Single" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Single</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="civilStatus" value="Married" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Married</span>
+    </label>
+  </div>
+  {/*religion */}
+    <div className="flex gap-2 items-center">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+            iv. Religion:-
+          </label>
+          <input
+            type="text"
+            name="religion"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+        <label htmlFor="dob" className="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+           v. Date of Birth :-
+        </label>
+        <input
+         type="date"
+         name="dob"
+         className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+       />
+     </div>
+        <div className="flex gap-2 items-center">
+          <label className="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+            vi. Age:-
+          </label>
+          <input
+            type="text"
+            name="age"
+            className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+          />
+        </div>
+        {/* Citizenship */}
+    <div class="flex items-center gap-4">
+     <label class="text-sm sm:text-lg xl:text-xl font-medium ml-10">
+       vii. Citizenship :-
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="citizen" value="Sri Lankan" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Sri Lankan</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="citizen" value="Foreigner" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">Foreigner</span>
+    </label>
+  </div> 
+  <div className="flex gap-2 items-center ml-10">
+      <label className="text-xs  ml-1">
+         (if foreigner please mention the name of the country)
+      </label>
+      <input
+        type="text"
+        name="ifforeigner"
+        className="border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-black px-3 py-1 text-sm sm:text-base xl:text-lg w-[200px] sm:w-[300px] xl:w-[400px]"
+      />
+       </div>
+       {/* if srilankan */}
+    <div class="flex items-center gap-4">
+     <label class="text-sm sm:text-lg xl:text-xl font-medium ml-100">
+       If Sri Lankan:-
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="srilankan" value="By Descent" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">By Descent</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input type="radio" name="srilankan" value="By Registration" class="border-gray-400" />
+      <span class="text-sm sm:text-base xl:text-lg font-medium">By Registration</span>
+    </label>
+  </div>  
+  <div className="flex gap-8 mt-2 sm:gap-20 sm:mt-8">
+            <SecondaryButton
+              text="Next"
+              color="bg-green-700"
+              hoverColor="hover:bg-green-800"
+            />
+   </div>
+</div>
+
+    </form>
+  );
 }
 
 export default A1Form_Part02;
