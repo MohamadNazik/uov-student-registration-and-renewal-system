231e009d242706f3c891b064458697dd4c1039b1
lasanga890 <lasangad890@gmail.com>
2025-01-12 20:59:06 +0530

    create UGC letter verify controller

---
+++ backend/controllers/User/DocumentsVerify/UGCLetterVerify.js
@@ -0,0 +1,39 @@
+export const olCertificateVerify = async (req, res) => {
+  try {
+    const { extractedText } = req;
+    if (!extractedText) {
+      return res
+        .status(400)
+        .send({ success: false, message: "No text provided for analysis" });
+    }
+
+    const lowerCaseExtractedText = extractedText.toLowerCase();
+    console.log(lowerCaseExtractedText);
+
+    const predefinedTexts = [
+      "university admission",
+      "university grant commission",
+    ];
+
+    const matches = predefinedTexts.filter((text) =>
+      lowerCaseExtractedText.includes(text)
+    );
+
+    matches.length > 0
+      ? res.status(200).send({
+          success: true,
+          message: "Verified UGC Letter",
+          matches,
+        })
+      : res.status(200).send({
+          success: false,
+          message: "Not verified",
+          matches,
+        });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
