6151e301101b5367daa78233fb282fae7e0ce4f4
lasanga890 <lasangad890@gmail.com>
2025-02-19 12:12:37 +0530

    create admin model

---
+++ backend/models/adminModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["deputy_registrar", "sar", "sar_staff", "far", "far_staff"],
+      required: true,
+    },
+    permissions: { type: [String], required: true },
+    faculty: { type: String },
+    eSignature: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Admin", adminSchema);
