e2ac6e1a8d006c57487f4e39c9b8cd0c0e8dfd51
shanusen27 <shashinianuththara20@gmail.com>
2025-02-20 21:54:40 +0530

    create controller to change user password (change their default password to own password)

---
+++ backend/controllers/User/changePasswordController.js
@@ -0,0 +1,54 @@
+import userModel from "../../models/userModel.js";
+import  bcrypt  from 'bcrypt';
+import { hashPassword } from "../../utils/hashPassword.js";
+
+export const changePasswordController = async (req, res) => {
+  try {
+    const { id, permanent_password, new_password, conform_password } = req.body;
+
+    if (!permanent_password || !new_password || !conform_password) {
+      return res.status(400).send({
+        success: false,
+        message: "All fields are required",
+      });
+    } else {
+      const student = await userModel.findById({ _id: id });
+      
+      if (!student){
+        res.status(404).send({
+            success: false,
+            message: "Student not found",
+        })
+      }else{
+       const isMatch = await bcrypt.compare(permanent_password,student.permanent_password)
+        if (!isMatch) {
+          return res.status(401).send({
+            success: false,
+            message: "Invalid current password",
+          });
+        }else{
+            if (new_password !== conform_password) {
+                return res.status(400).send({
+                  success: false,
+                  message: "New password and confirm password do not match",
+                });
+            }else{
+                 const hashedPassword = await hashPassword(new_password);
+               const result =  await student.updateOne({permanent_password: hashedPassword});
+                return res.status(200).send({
+                  success: true,
+                  message: "Password updated successfully",
+                  student
+                });
+            }
+        }
+      }
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
