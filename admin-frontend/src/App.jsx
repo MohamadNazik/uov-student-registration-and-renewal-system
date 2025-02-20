cd38c89a10d5868d86969b619f22c5ec58126273
Nazik <shmnazik79@gmail.com>
2025-02-21 01:39:06 +0530

    admin frontend some file structure created. routing added for 2 pages.

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  { path: "/", element: <LoginPage /> },
+  { path: "/dashboard", element: <Dashboard /> },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ admin-frontend/src/components/StudentIDCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StudentIDCard() {
+  return <div>StudentIDCard</div>;
+}
+
+export default StudentIDCard;
