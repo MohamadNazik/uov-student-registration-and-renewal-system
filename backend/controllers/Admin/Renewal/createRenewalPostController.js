07abcda910551c627046ceb90cf1baa056215655
Sumy2001 <154615028+Sumy2001@users.noreply.github.com>
2025-02-22 18:39:16 +0530

    create controller to post renewal announcement

---
+++ backend/controllers/Admin/Renewal/createRenewalPostController.js
@@ -0,0 +1,52 @@
+import adminUpdatesModel from "../../../models/adminUpdatesModel.js";
+
+export const createRenewalPostController = async (req, res) => {
+  try {
+    const {
+      adminId,
+      updateType,
+      updateMessage,
+      expireDate,
+      academicYear,
+    } = req.body;
+
+    if (
+      !updateType ||
+      !updateMessage ||
+      !expireDate ||
+      !academicYear
+    ) {
+      return res.status(400).send({
+        success: false,
+        message: "All fields are required",
+      });
+    } else {
+      if (updateType !== "Renewal") {
+        return res.status(400).send({
+          success: false,
+          message: "Invalid update type",
+        });
+      } else {
+        const registrationPost = new adminUpdatesModel({
+          adminId,
+          updateType,
+          updateMessage,
+          expireDate,
+          academicYear,
+        });
+        await registrationPost.save();
+        return res.status(200).send({
+          success: true,
+          message: "Renewal post created successfully",
+          registrationPost,
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message,
+      message: "Error creating Renewal post",
+    });
+  }
+};
