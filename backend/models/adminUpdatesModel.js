2d216d969142f9e1adbb63efba0543d0077ae3f6
lasanga890 <lasangad890@gmail.com>
2025-02-23 20:24:08 +0530

    fix bugs in verify student controller

--- backend/models/adminUpdatesModel.js
+++ backend/models/adminUpdatesModel.js
@@ -1,37 +1,38 @@
 import mongoose from "mongoose";
 
-const adminUpdatesSchema = new mongoose.Schema({
-  adminId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Admin",
-    required: true,
+const adminUpdatesSchema = new mongoose.Schema(
+  {
+    adminId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    updateType: {
+      type: String,
+      enum: ["Registration", "Renewal"],
+      required: true,
+    },
+    updateMessage: {
+      type: String,
+      required: true,
+    },
+    expireDate: {
+      type: Date,
+      required: true,
+    },
+    enrollmentDate: {
+      type: Date,
+      required: false,
+    },
+    idCardIssueDate: {
+      type: Date,
+      required: false,
+    },
+    academicYear: {
+      type: [String],
+      required: true,
+    },
   },
-  updateType:{
-    type: String,
-    enum:['Registration','Renewal'],
-    required: true,
-  },
-  updateMessage:{
-    type: String,
-    required: true,
-  },
-  expireDate:{
-    type: Date,
-    required: true,
-  },
-  enrollmentDate:{
-    type: Date,
-    required: false,
-  },
-  idCardIssueDate:{
-    type: Date,
-    required: false,
-  },
-  academicYear:{
-    type: [String],
-    required: true,
-  },
-
-},
-{ timestamps: true });
-export default mongoose.model('adminUpdates',adminUpdatesSchema);
+  { timestamps: true }
+);
+export default mongoose.model("adminUpdates", adminUpdatesSchema);
--- backend/routes/common/commonRoute.js
+++ backend/routes/common/commonRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { forgotPasswordController, resendOtpController,verifyOTPController,resetPasswordController } from "../../controllers/common/forgotPasswordController.js";
+import { checkRegistrationAvailabilityController } from "../../controllers/common/checkRegistrationAvailabilityController.js";
 
 const router = express.Router();
 
@@ -7,5 +8,7 @@ router.post("/forgot-password", forgotPasswordController);
 router.post('/verify-otp',verifyOTPController);
 router.post('/resend-otp',resendOtpController);
 router.post('/reset-password',resetPasswordController);
+
+router.post('/registration-available',checkRegistrationAvailabilityController);
 
 export default router;
