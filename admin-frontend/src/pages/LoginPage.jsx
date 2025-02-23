b54a2c1276038ee2b49c56ec404ded427850a5f9
maleesha <maleesharathnayake840@gmail.com>
2025-02-23 23:35:25 +0530

    Finish frontend in admin-Login Page

--- admin-frontend/src/pages/LoginPage.jsx
+++ admin-frontend/src/pages/LoginPage.jsx
@@ -14,15 +14,15 @@ function LoginPage() {
         <div className="relative">
           <input
             type="text"
-            id="regno"
+            id="username"
             className="block px-2 pb-2 sm:px-2.5 sm:pb-2.5 pt-4 w-[14rem] sm:w-[20rem] text-xs sm:text-lg font-medium text-gray-900 bg-transparent outline outline-2 outline-gray-300 rounded-md peer focus:outline focus:outline-2 focus:outline-black"
             placeholder=" "
           />
           <label
-            htmlFor="regno"
+            htmlFor="username"
             className="absolute text-xs sm:text-lg text-gray-500 duration-300 transform -translate-y-4 sm:-translate-y-5 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 sm:peer-focus:-translate-y-5 start-1 cursor-text"
           >
-            Registration Number
+            UserName
           </label>
         </div>
         <div className="relative">
