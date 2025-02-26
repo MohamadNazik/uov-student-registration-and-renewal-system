4d911759eadef5494ea3699e2dda14f9e48e6c0f
maleesha <maleesharathnayake840@gmail.com>
2025-02-26 15:46:04 +0530

    create Admin Dashborad

--- admin-frontend/src/pages/Dashboard.jsx
+++ admin-frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import TextCard from "../components/TextCard";
+import PrimaryButton from "../components/PrimaryButton";
+import { Link } from "react-router-dom";
+
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+
+
+  return (
+    <div><Header title="Admin Dashboard" />
+      <div className="flex flex-col items-center gap-5 sm:gap-8 mt-8 sm:mt-12">
+
+
+
+        <Link to="/change-password">
+          <PrimaryButton text="Change Password" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
