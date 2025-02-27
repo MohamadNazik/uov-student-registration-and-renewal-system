a37624e4ecb4f6a39fb01f41e6dcbc0290c7725b
lasanga890 <lasangad890@gmail.com>
2025-02-27 23:40:06 +0530

    `Added NewRegistrations page and updated routes in App.jsx and Dashboard.jsx`

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
+import NewRegistrations from "./pages/NewRegistrations";
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/dashboard", element: <Dashboard /> },
+  {path:'/new-registrations',element:<NewRegistrations />}
 ]);
 
 function App() {
---
+++ admin-frontend/src/pages/NewRegistrations.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/Header";
+
+function NewRegistrations() {
+  return <div>
+    <Header title="New Registrations" />
+  </div>;
+}
+
+export default NewRegistrations;
