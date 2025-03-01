b66e2f7ffd52445ec35676fa880c08adf7d99beb
Nazik <shmnazik79@gmail.com>
2025-03-02 01:14:17 +0530

    Added loading state and loading component to VerifyID page

--- student-frontend/src/pages/RegistrationProcess/VerifyID.jsx
+++ student-frontend/src/pages/RegistrationProcess/VerifyID.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SecondaryButton from "../../components/SecondaryButton";
 import StudentIDCard from "../../components/StudentIDCard";
 import { useFormContext } from "../../utils/FormContext";
 import axios from "axios";
 import { openDB } from "idb";
+import Loading from "../../components/Loading";
 
 function VerifyID() {
   const { formData } = useFormContext();
+
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   // console.log(formData);
@@ -72,6 +75,8 @@ function VerifyID() {
       if (formData.signature)
         submissionData.append("signature", formData.signature);
 
+      setIsLoading(true);
+
       const response = await axios.post(
         "http://localhost:8080/api/users/add-student",
         submissionData,
@@ -82,6 +87,7 @@ function VerifyID() {
 
       if (response.data.success) {
         await clearIndexedDBFiles();
+        setIsLoading(false);
 
         sessionStorage.removeItem("formData");
         localStorage.removeItem("student");
@@ -91,56 +97,60 @@ function VerifyID() {
       }
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="mb-10">
-      <div className="bg-white md:block sm:flex m-2 sm:m-5 xl:ml-8 p-2 sm:p-7 xl:p-10 shadow-md rounded-lg  justify-center">
-        {/* Details of the parents/Guardians  */}
-        <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
-          <label className="text-sm font-bold sm:text-lg xl:text-2xl uppercase">
-            9. Student id details verification
-          </label>
-          <span className="ml-8 text-red-600 font-medium text-md">
-            This will be your Student ID card of UNIVERSITY OF VAVUNIYA. Check
-            whether your details are correct or not.
-          </span>
+    <>
+      <div className="mb-10">
+        <div className="bg-white md:block sm:flex m-2 sm:m-5 xl:ml-8 p-2 sm:p-7 xl:p-10 shadow-md rounded-lg  justify-center">
+          {/* Details of the parents/Guardians  */}
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm font-bold sm:text-lg xl:text-2xl uppercase">
+              9. Student id details verification
+            </label>
+            <span className="ml-8 text-red-600 font-medium text-md">
+              This will be your Student ID card of UNIVERSITY OF VAVUNIYA. Check
+              whether your details are correct or not.
+            </span>
+          </div>
+        </div>
+
+        {/* student id */}
+        <div className="flex w-full justify-center py-5 xl:py-16">
+          <StudentIDCard
+            image={formData.profile_photo}
+            Name_with_Initials={formData.Name_with_Initials}
+            Enrollment_Number={formData.Enrollment_Number}
+            Address={formData.Address.Permenant_Address}
+            NIC={formData.Address.NIC}
+            Enrollment_Date={formData.Enrollment_Date}
+            Date_of_Issue={formData.ID_IssueDate}
+            Acedamic_Year={formData.AcademicYear}
+            stSignature={formData.signature}
+          />
+        </div>
+
+        {/* Buttons */}
+        <div className="flex gap-8 mt-2 sm:gap-10 sm:mt-8 justify-center">
+          <Link to="/a1-from-part-1">
+            <SecondaryButton
+              text="Not Correct"
+              color="bg-red-700"
+              hoverColor="hover:bg-green-800"
+            />
+          </Link>
+          <SecondaryButton
+            text="Correct & Submit"
+            color="bg-green-700"
+            hoverColor="hover:bg-green-800"
+            onClick={handleSubmit}
+          />
         </div>
       </div>
-
-      {/* student id */}
-      <div className="flex w-full justify-center py-5 xl:py-16">
-        <StudentIDCard
-          image={formData.profile_photo}
-          Name_with_Initials={formData.Name_with_Initials}
-          Enrollment_Number={formData.Enrollment_Number}
-          Address={formData.Address.Permenant_Address}
-          NIC={formData.Address.NIC}
-          Enrollment_Date={formData.Enrollment_Date}
-          Date_of_Issue={formData.ID_IssueDate}
-          Acedamic_Year={formData.AcademicYear}
-          stSignature={formData.signature}
-        />
-      </div>
-
-      {/* Buttons */}
-      <div className="flex gap-8 mt-2 sm:gap-10 sm:mt-8 justify-center">
-        <Link to="/a1-from-part-1">
-          <SecondaryButton
-            text="Not Correct"
-            color="bg-red-700"
-            hoverColor="hover:bg-green-800"
-          />
-        </Link>
-        <SecondaryButton
-          text="Correct & Submit"
-          color="bg-green-700"
-          hoverColor="hover:bg-green-800"
-          onClick={handleSubmit}
-        />
-      </div>
-    </div>
+      {isLoading && <Loading />}
+    </>
   );
 }
 
