3fc854a6559869dd46dbd2b169a0f2b96777d0a5
maleesha <maleesharathnayake840@gmail.com>
2025-03-04 21:24:27 +0530

    Add route for App.jsx

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -3,12 +3,20 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import NewRegistrations from "./pages/NewRegistrations";
 import RegisteredStudents from "./pages/RegisteredStudents";
+import FirstYear from "./pages/FirstYear";  
+import SecondYear from "./pages/SecondYear";  
+import ThirdYear from "./pages/ThirdYear";
+import FourthYear from "./pages/FourthYear";  
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/dashboard", element: <Dashboard /> },
-  {path:'/new-registrations',element:<NewRegistrations />},
-  {path:'/registered-students',element:<RegisteredStudents/>}
+  { path:'/new-registrations',element:<NewRegistrations />},
+  { path:'/registered-students',element:<RegisteredStudents />},
+  { path: "/first-year", element: <FirstYear /> },
+  { path: "/second-year", element: <SecondYear /> },
+  { path: "/third-year", element: <ThirdYear /> },
+  { path: "/fourth-year", element: <FourthYear /> },
 ]);
 
 function App() {
--- admin-frontend/src/pages/RegisteredStudents.jsx
+++ admin-frontend/src/pages/RegisteredStudents.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import Header from "../components/Header";
 import TextCard from "../components/TextCard";
-import PrimaryButton from "../components/PrimaryButton";
 import { Link } from "react-router-dom";
 
 function UserDashboard() {
-  return (
-    <div>
-      <Header title="Registered Students Dashboard" />
-      <div className="flex flex-col items-center gap-5 sm:gap-8 mt-8 sm:mt-12">
-        <Link to="/first-year">
-          <TextCard text="For First Year" />
-        </Link>
-        <Link to="/second-year">
-          <TextCard text="For Second Year" />
-        </Link>
-        <Link to="/third-year">
-          <TextCard text="For Third Year" />
-        </Link>
-        <Link to="/fourth-year">
-          <PrimaryButton text="For Fourh Year" />
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Header title="Registered Students Dashboard" />
+            <div className="flex flex-col items-center gap-5 sm:gap-8 mt-8 sm:mt-12">
+                <Link to="/first-year">
+                    <TextCard text="For First Year" />
+                </Link>
+                <Link to="/second-year">
+                    <TextCard text="For Second Year" />
+                </Link>
+                <Link to="/third-year">
+                    <TextCard text="For Third Year" />
+                </Link>
+                <Link to="/fourth-year">
+                    <TextCard text="For Fourth Year" />
+                </Link>
+            </div>
+        </div>
+    );
 }
