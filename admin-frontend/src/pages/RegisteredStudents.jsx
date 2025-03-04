a22ffbc042bab07e2dfe1fa443811929770b0c80
maleesha <maleesharathnayake840@gmail.com>
2025-03-04 21:59:11 +0530

    Add Goto dashbord button to registredStudent dashboard

--- admin-frontend/src/pages/RegisteredStudents.jsx
+++ admin-frontend/src/pages/RegisteredStudents.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import TextCard from "../components/TextCard";
 import { Link } from "react-router-dom";
+import PrimaryButton from "../components/PrimaryButton";
 
 function RegisteredStudents() {
     return (
@@ -9,7 +10,7 @@ function RegisteredStudents() {
             <Header title="Registered Students Dashboard" />
             <div className="flex flex-col items-center gap-5 sm:gap-8 mt-8 sm:mt-12">
                 <Link to="/first-year">
-                    <TextCard text="For First Year" />
+                    <TextCard  text="For First Year  " />
                 </Link>
                 <Link to="/second-year">
                     <TextCard text="For Second Year" />
@@ -19,6 +20,9 @@ function RegisteredStudents() {
                 </Link>
                 <Link to="/fourth-year">
                     <TextCard text="For Fourth Year" />
+                </Link>
+                <Link to="/fourth-year">
+                    <PrimaryButton text="Go To Dashboard" />
                 </Link>
             </div>
         </div>
