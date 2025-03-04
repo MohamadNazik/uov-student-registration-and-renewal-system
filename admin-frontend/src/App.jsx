a356c009235a51fb3858b87304b476f578dbf52d
maleesha <maleesharathnayake840@gmail.com>
2025-03-04 21:09:12 +0530

    add route to app.jsx

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import NewRegistrations from "./pages/NewRegistrations";
+import RegisteredStudents from "./pages/RegisteredStudents";
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginPage /> },
   { path: "/dashboard", element: <Dashboard /> },
-  {path:'/new-registrations',element:<NewRegistrations />}
+  {path:'/new-registrations',element:<NewRegistrations />},
+  {path:'/new-registrations',element:<RegisteredStudents/>}
 ]);
 
 function App() {
